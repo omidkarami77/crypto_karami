f0894373b8252e8d91a20f12b845a1a0e735dc9a
omidkarami77 <omidtkd77@gmail.com>
2022-06-01 13:42:24 +0330

    add refresh indicator to list

--- lib/coin_list_screen.dart
+++ lib/coin_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:crypto_karami/data/constant/constants.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'data/model/crypto.dart';
@@ -23,74 +24,87 @@ class _CoinListScreenState extends State<CoinListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'karami  crypto',
-            style: TextStyle(fontFamily: 'omidfont'),
-          ),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          backgroundColor: blackColor,
+      appBar: AppBar(
+        title: Text(
+          'karami  crypto',
+          style: TextStyle(fontFamily: 'omidfont'),
         ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
         backgroundColor: blackColor,
-        body: SafeArea(
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              var change = cryptos![index].changePercent24Hr;
-              var twonum = cryptos![index].priceUsd.toString();
+      ),
+      backgroundColor: blackColor,
+      body: SafeArea(
+        child: RefreshIndicator(
+            color: blackColor,
+            backgroundColor: greenColor,
+            onRefresh: () async {
+              List<Crypto>? freshData = await getDataCrypto();
 
-              double changeper = double.parse(change!);
-              double priceUsdDouble = double.parse(twonum);
-              return ListTile(
-                title: Text(
-                  '${cryptos![index].name}',
-                  style: TextStyle(color: greenColor),
-                ),
-                subtitle: Text(
-                  '${cryptos![index].symbol}',
-                  style: TextStyle(color: greyColor),
-                ),
-                leading: SizedBox(
-                  width: 30,
-                  child: Center(
-                    child: Text(
-                      '${cryptos![index].rank.toString()}',
-                      style: TextStyle(color: greyColor),
-                    ),
-                  ),
-                ),
-                trailing: SizedBox(
-                  width: 150,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            '${priceUsdDouble.toStringAsFixed(2)}',
-                            style: TextStyle(color: greyColor,fontSize: 18),
-                          ),
-                          Text(
-                            '${changeper.toStringAsFixed(2)}',
-                            style: TextStyle(
-                                color: getColorChangePercent(changeper)),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                          width: 50,
-                          child:
-                              Center(child: getIconChangePercent(changeper))),
-                    ],
-                  ),
-                ),
-              );
+              setState(() {
+                cryptos = freshData;
+              });
             },
-            itemCount: cryptos!.length,
+            child: getListView()),
+      ),
+    );
+  }
+
+  Widget getListView() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        var change = cryptos![index].changePercent24Hr;
+        var twonum = cryptos![index].priceUsd.toString();
+
+        double changeper = double.parse(change!);
+        double priceUsdDouble = double.parse(twonum);
+        return ListTile(
+          title: Text(
+            '${cryptos![index].name}',
+            style: TextStyle(color: greenColor),
           ),
-        ));
+          subtitle: Text(
+            '${cryptos![index].symbol}',
+            style: TextStyle(color: greyColor),
+          ),
+          leading: SizedBox(
+            width: 30,
+            child: Center(
+              child: Text(
+                '${cryptos![index].rank.toString()}',
+                style: TextStyle(color: greyColor),
+              ),
+            ),
+          ),
+          trailing: SizedBox(
+            width: 150,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '${priceUsdDouble.toStringAsFixed(2)}',
+                      style: TextStyle(color: greyColor, fontSize: 18),
+                    ),
+                    Text(
+                      '${changeper.toStringAsFixed(2)}',
+                      style: TextStyle(color: getColorChangePercent(changeper)),
+                    )
+                  ],
+                ),
+                SizedBox(
+                    width: 50,
+                    child: Center(child: getIconChangePercent(changeper))),
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: cryptos!.length,
+    );
   }
 
   Widget getIconChangePercent(double percentChange) {
@@ -109,5 +123,15 @@ class _CoinListScreenState extends State<CoinListScreen> {
 
   Color getColorChangePercent(double percentChange) {
     return percentChange <= 0 ? redColor : greenColor;
+  }
+
+  Future<List<Crypto>?> getDataCrypto() async {
+    var response = await Dio().get('https://api.coincap.io/v2/assets');
+
+    List<Crypto> cryptos = response.data['data']
+        .map<Crypto>((data) => Crypto.fromJson(data))
+        .toList();
+
+    return cryptos;
   }
 }
